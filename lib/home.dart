6661253ffcbbba016a38c83cc7cf661b153ccad9
Rozza <sim.LoayIssam23106@alexu.edu.eg>
2024-08-01 02:08:46 +0300

    second

--- lib/home.dart
+++ lib/home.dart
@@ -23,23 +23,23 @@ class HomeView extends StatelessWidget {
               const DoctorWidget(),
               const InputWidget(),
               Hospital(),
-              DoctorCard(
+              const DoctorCard(
                 name: 'Dr. Joseph Brostito',
                 specialty: 'Dental Specialist',
                 distance: '1.2 KM',
                 rating: 4.8,
                 reviews: 120,
                 openingTime: '17.00',
-                imagePath: 'assets/images/image.png', // Provide the path to your asset image
+                imagePath: 'assets/images/image.png',
               ),
-              DoctorCard(
+              const DoctorCard(
                 name: 'Dr. Imran Syahir',
                 specialty: 'General Doctor',
                 distance: '1.2 KM',
                 rating: 5,
                 reviews: 100,
                 openingTime: '20.00',
-                imagePath: 'assets/images/profile.png', // Provide the path to your asset image
+                imagePath: 'assets/images/profile.png',
               ),
               // Add more DoctorCards here as needed
             ],
